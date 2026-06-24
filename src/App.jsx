0d29f8062bf5d7e4e3e26589e06e8729f26f6f95
khisa318 <khisaarmstrong@gmail.com>
2026-06-24 11:39:39 +0300

    feat: add AudioToText, VideoToText, and SubtitleGenerator components to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from './pages/Home';
 import  {Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import NotFoundPage from './pages/NotFoundPage';
+import AudioToText from './pages/AudioToText';
+import VideoToText from './pages/VideoToText';
+import SubtitleGenerator from './pages/SubtitleGenerator';
 
 function App(){
   return(
